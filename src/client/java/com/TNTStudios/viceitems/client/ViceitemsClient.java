e45d0372ff985476bbe841f318256212c00a774a
TNTStudios <tntstudiosn@gmail.com>
2025-05-25 08:49:52 -0600

    Initial commit

---
+++ src/client/java/com/TNTStudios/viceitems/client/ViceitemsClient.java
@@ -0,0 +1,10 @@
+package com.TNTStudios.viceitems.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ViceitemsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/TNTStudios/viceitems/Viceitems.java
@@ -0,0 +1,10 @@
+package com.TNTStudios.viceitems;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Viceitems implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
