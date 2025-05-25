475765422e351b0526fe2c34ede69125b0f100e2
TNTStudios <tntstudiosn@gmail.com>
2025-05-25 09:21:03 -0600

    1

--- src/client/java/com/TNTStudios/viceitems/client/ViceitemsClient.java
+++ src/client/java/com/TNTStudios/viceitems/client/ViceitemsClient.java
@@ -1,10 +1,13 @@
 package com.TNTStudios.viceitems.client;
 
+import com.TNTStudios.viceitems.Viceitems;
+import com.TNTStudios.viceitems.client.renderer.CameraRenderer;
+import com.TNTStudios.viceitems.item.CameraItem;
 import net.fabricmc.api.ClientModInitializer;
+import software.bernie.geckolib.renderer.GeoItemRenderer;
 
 public class ViceitemsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        // Ya no se necesita registrar renderers aquí para GeoItems
     }
 }
